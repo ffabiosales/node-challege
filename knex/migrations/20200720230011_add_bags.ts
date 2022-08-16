53830ca8f46fbfac47198dec186ec4b4cc23e7b0
Fábio Sales <ffabiosales@gmail.com>
2022-08-16 03:59:42 -0300

    add missing columns

--- knex/migrations/20200720230011_add_bags.ts
+++ knex/migrations/20200720230011_add_bags.ts
@@ -6,6 +6,10 @@ export const up = (knex: Knex): Promise<void> =>
     table.increments();
     table.timestamps();
     table.integer('volume');
+    table.string('title');
+    table.integer('payloadVolume');
+    table.integer('availableVolume');
+    table.enu('cuboids', []);
   });
 
 export const down = (knex: Knex): Promise<void> =>
